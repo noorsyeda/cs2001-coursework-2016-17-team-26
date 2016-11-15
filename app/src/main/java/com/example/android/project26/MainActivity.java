49490e75352b72791688a55f0301a8bf13fc8586
alex <a.cealicov@gmail.com>
2016-11-15 17:31:22 +0000

    project upload

---
+++ app/src/main/java/com/example/android/project26/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.project26;
+
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cam_but(View view) {
+
+    }
+}
