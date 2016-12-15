e811de3960946a00e215664d140c335689f34c79
alex <a.cealicov@gmail.com>
2016-12-15 04:49:18 +0000

    contains errors TODO: integrate camera into a tab

---
+++ app/src/main/java/com/example/android/project26/GalleryActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.project26;
+
+/**
+ * Created by home on 14/12/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GalleryActivity extends Fragment{
+
+    public GalleryActivity() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_gallery, container, false);
+    }
+
+}
