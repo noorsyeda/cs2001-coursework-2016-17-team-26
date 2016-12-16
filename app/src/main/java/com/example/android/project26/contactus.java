8efb3f30fda53d91e44b7b5330f3d5a69609a462
Dawood Ahmed <dawood1601@users.noreply.github.com>
2016-12-16 18:06:05 +0000

    Initialising layout contactus

---
+++ app/src/main/java/com/example/android/project26/contactus.java
@@ -0,0 +1,17 @@
+package com.example.android.project26;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dawoo on 16/12/2016.
+ */
+
+public class contactus extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
