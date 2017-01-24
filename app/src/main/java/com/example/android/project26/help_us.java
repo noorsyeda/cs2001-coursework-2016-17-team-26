391862cd8f4319e22ba32fbfe99a3f8118c51b10
Dawood Ahmed <dawood1601@users.noreply.github.com>
2017-01-24 10:53:39 +0000

    java file for help us is created in Menu

---
+++ app/src/main/java/com/example/android/project26/help_us.java
@@ -0,0 +1,38 @@
+package com.example.dawoo.menudemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dawoo on 16/12/2016.
+ */
+
+public class help_us extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_us);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+}
+
