ed339d11e567286114b87490082d390b1feaa278
alex <a.cealicov@gmail.com>
2017-01-27 16:24:07 +0000

    Merged menus(by dawood) with master

--- app/src/main/java/com/example/android/project26/MainActivity.java
+++ app/src/main/java/com/example/android/project26/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.project26;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
@@ -9,6 +10,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +43,49 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FF0000"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.contact_us_id:
+                contact();
+                return true;
+            case R.id.about_us_id:
+                about();
+                return true;
+            case R.id.settings_id:
+                setting();
+                return true;
+
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public void contact()
+    {
+        Intent intent = new Intent(MainActivity.this, contactus.class);
+        startActivity(intent);
+    }
+    public void about()
+    {
+        Intent intent = new Intent(MainActivity.this, about_us.class);
+        startActivity(intent);
+    }
+    public void setting()
+    {
+        Intent intent = new Intent(MainActivity.this, setting_us.class);
+        startActivity(intent);
     }
 
     private void setupViewPager(ViewPager viewPager) {
